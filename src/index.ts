bb7cfea2c92bc695f63cc4a71007d721f6c57230
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-08-11 12:28:35 +0530

    1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 const app=express();
 import {UserRouter} from "./routes/user.route";
+import config from "config";
+console.log("Mongo url",config.get("db.url"));
+// NODE_ENV=production node src/index.ts
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
